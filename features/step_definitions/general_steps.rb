795fc9eff744593e9424565384d440888e4df9c3
Matthijs Groen <matthijs.groen@gmail.com>
2014-04-30 06:46:13 +0200

    Create step definitions

---
+++ features/step_definitions/general_steps.rb
@@ -0,0 +1,32 @@
+Stel(/^ik heb een speelbord met (\d+) vakjes$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Stel(/^ik heb de volgende spelers met de klok mee:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Stel(/^alle pionnen staan op het startvakje$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Dan(/^is Piet aan de beurt om te dobbelen omdat hij de jongste speler is$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Als(/^de beurt van Piet is geweest$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Dan(/^is Klaas aan de beurt om te dobbelen$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Als(/^de beurt van Klaas is geweest$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Dan(/^is Jan aan de beurt om te dobbelen$/) do
+  pending # express the regexp above with the code you wish you had
+end
