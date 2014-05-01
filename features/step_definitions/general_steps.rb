d8052258cbe152f4a135dfcc3cce1d6ac55ee504
Matthijs Groen <matthijs.groen@gmail.com>
2014-05-02 00:17:06 +0200

    Setup assertions of second scenario

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -33,17 +33,17 @@ Dan(/^is Piet aan de beurt om te dobbelen omdat hij de jongste speler is$/) do
 end
 
 Als(/^de beurt van Piet is geweest$/) do
-  pending # express the regexp above with the code you wish you had
+  # No assertion at the moment that requires implementation here
 end
 
 Dan(/^is Klaas aan de beurt om te dobbelen$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@game.active_player.name).to eql 'Klaas'
 end
 
 Als(/^de beurt van Klaas is geweest$/) do
-  pending # express the regexp above with the code you wish you had
+  # No assertion at the moment that requires implementation here
 end
 
 Dan(/^is Jan aan de beurt om te dobbelen$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@game.active_player.name).to eql 'Jan'
 end
