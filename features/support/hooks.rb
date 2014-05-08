ce635a1feb4a067792ee8382ef73c948e2e776e1
Matthijs Groen <matthijs.groen@gmail.com>
2014-05-08 08:27:39 +0200

    Build code once for all tests

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,8 +1,8 @@
-Before do
-  require 'rake'
-  load File.expand_path('../../../tasks/build.rake', __FILE__)
-  Rake::Task['build'].invoke
+require 'rake'
+load File.expand_path('../../../tasks/build.rake', __FILE__)
+Rake::Task['build'].invoke
 
+Before do
   visit 'dist/index.html'
   ModelProxy.session = self
 end
