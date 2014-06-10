d9f9cff5825e2b88c79ff2f93a5cca22e75aa453
Matthijs Groen <matthijs.groen@gmail.com>
2014-06-10 12:34:04 +0200

    Start rendering of board

--- features/support/demo_mode.rb
+++ features/support/demo_mode.rb
@@ -10,12 +10,15 @@ class DemoRenderer
 
   def initialize(game)
     @game = game
+
+    @board_window = Window.new(lines - 14, cols * (2 / 3.0), 12, 0)
   end
 
   def render
     return unless game
 
     render_players
+    render_board
     refresh
   end
 
@@ -34,5 +37,59 @@ class DemoRenderer
       setpos(5 + i, 4)
       addstr("- #{p.name} (#{p.age}) => #{p.pawn.color}")
     end
+  end
+
+  def render_board
+    @board_window.clear
+    return unless game.board
+
+    # spaces = game.board.space_count
+    spaces = 31
+
+    @board_window.box('|', '-')
+    @board_window.setpos(@board_window.maxy - 5, 3)
+    startx, starty = @board_window.curx, @board_window.cury
+    endx, endy = startx, starty
+    direction = :right
+
+    (0..spaces).each do |i|
+      contents = space_contents(i)
+      direction = :top if endx + contents.length + 1 > @board_window.maxx
+
+      if direction == :right
+        startx = endx + 1
+      elsif direction == :top
+        starty -=  4
+      end
+      @board_window.setpos(starty, startx)
+      @board_window.addstr(contents)
+      endx, endy = @board_window.curx, @board_window.cury
+
+      # surround with box
+      draw_box(startx, starty, endx, endy)
+    end
+
+    @board_window.refresh
+  end
+
+  def space_contents(index)
+    if index == 0
+      '  start  '
+    else
+      format('  %2d  ', index)
+    end
+  end
+
+  def draw_box(startx, starty, endx, endy)
+    @board_window.setpos(starty - 2, startx)
+    @board_window.addstr('-' * (endx - startx))
+    (starty - 1 .. endy + 1).each do |y|
+      @board_window.setpos(y, startx - 1)
+      @board_window.addstr('|')
+      @board_window.setpos(y, endx)
+      @board_window.addstr('|')
+    end
+    @board_window.setpos(endy + 2, startx)
+    @board_window.addstr('-' * (endx - startx))
   end
 end
