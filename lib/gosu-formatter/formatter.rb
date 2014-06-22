2c2a318b0baf6ff313a2f92b8e6ca6a33b25d24a
Matthijs Groen <matthijs.groen@gmail.com>
2014-06-22 11:17:32 +0200

    Listen to die rolls and pawn moves

--- lib/gosu-formatter/formatter.rb
+++ lib/gosu-formatter/formatter.rb
@@ -11,14 +11,60 @@ module Cucumber
         attr_accessor :instance
 
         def update_game(game)
+          @game = game
+          add_game_hooks(game)
+          send_game_struct_to_view
+        end
+
+        def update
+          send_game_struct_to_view
+        end
+
+        private
+
+        attr_reader :game
+
+        # tell gosu when die is rolled
+        module DieListener
+          attr_accessor :formatter_listener
+
+          def roll
+            super.tap { formatter_listener.update }
+          end
+        end
+
+        # tell gosu when pawn is moved
+        module PawnListener
+          attr_accessor :formatter_listener
+
+          def location=(_value)
+            super.tap { formatter_listener.update }
+          end
+        end
+
+        def add_game_hooks(game)
+          return unless game
+          add_listener(game.die, DieListener)
+          game.players.each do |p|
+            add_listener(p.pawn, PawnListener)
+          end
+        end
+
+        def add_listener(obj, listener)
+          return unless obj
+          obj_modules = (class << obj; self; end).included_modules
+          return if obj_modules.include? listener
+          obj.extend listener
+          obj.formatter_listener = self
+        end
+
+        def send_game_struct_to_view
           game_struct = create_game_struct(game)
           with_remote_game do |remote|
             remote.game_struct = game_struct
           end
           sleep 0.5
         end
-
-        private
 
         def with_remote_game
           @remotegame ||= DRbObject.new_with_uri('druby://localhost:8787')
